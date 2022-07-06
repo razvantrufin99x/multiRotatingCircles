86c265da2297657296a748989d32b0d810461ec0
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-07-06 03:19:51 +0300

    Add project files.

---
+++ multiRotatingCircles/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace multiRotatingCircles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(926, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 169);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(926, 179);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 169);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 753);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ multiRotatingCircles/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace multiRotatingCircles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        Pen pen0 = new Pen(Color.Black,1);
+        Pen pen2 = new Pen(Color.Red, 1);
+        Pen pen1 = new Pen(Color.Silver, 1);
+        float rad = (float)(180 / Math.PI);
+        int cux = 350;
+        int cuy = 350;
+
+        bool eprim(int x)
+        {
+            bool esteprim = true;
+
+            /*
+            int d = 0;
+
+            while (d <= (int)Math.Sqrt(x) && esteprim == true)
+            {
+                if (x % d == 0) { esteprim = false; }
+                else if (d == 2) { d = d + 1; }
+                else { d = d + 2; }
+            }
+            */
+
+            for (int i = 2; i < (int)Math.Sqrt(x); i+=1)
+            {
+                if (x % i == 0) { esteprim = false; }
+            }
+
+            return esteprim;
+
+        
+        }
+
+        void drawCircle()
+        {
+
+            float px, py, cx, cy;
+            cx = (float)Math.Cos(0 / rad) * (200 + 0) + cux;
+            cy = (float)Math.Sin(0 / rad) * (200 + 0) + cuy;
+            px = cx;
+            py = cy;
+            int p = 0;
+            this.Width = 10000;
+            this.Height = 10000;
+            
+
+                for (float j = 0; j < 360; j += 1.0f)
+                {
+                    for (float i = 0; i < 360; i += 1.0f)
+                    {
+                        p++;
+                        if (eprim(p) != true)
+                        {
+                            px = cx;
+                            py = cy;
+                            cx = (float)Math.Cos(i / rad) * (200 + j) + cux;
+                            cy = (float)Math.Sin(i / rad) * (200 + j) + cuy;
+                           // g.DrawLine(pen1, cx, cy, px, py);
+                            //g.DrawRectangle(pen0, cx, cy, 1, 1);
+                            //textBox1.Text += "\r\n";
+                            //textBox1.Text += p.ToString();
+
+                        }
+                        if (eprim(p) == true)
+                        {
+                            px = cx;
+                            py = cy;
+                            cx = (float)Math.Cos(i / rad) * (200 + j) + cux;
+                            cy = (float)Math.Sin(i / rad) * (200 + j) + cuy;
+                           // g.DrawLine(pen1, cx, cy, px, py);
+                            g.DrawRectangle(pen2, cx, cy, 1, 1);
+                            //textBox2.Text += "\r\n";
+                            //textBox2.Text += p.ToString();
+                        }
+                    }
+                   
+               }
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            g = CreateGraphics();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            drawCircle();
+           //timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            
+            //drawCircle();
+            //g.ScaleTransform(-1.0f, -1.0f);
+            //g.RotateTransform(-0.250f);
+            //g.TranslateTransform(-1.0f, -1.0f);
+            
+        }
+
+
+    }
+}
